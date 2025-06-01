b0613fc7c8198b727d78f3dc35d778ce42051687
NhalegeLLC <Nhalege@icloud.com>
2025-06-01 19:02:21 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+cd nhalege-media-ai
+git init
+git add .
+git commit -m "Initial commit for Nhalege Media AI"
+git branch -M main
+git remote add origin https://github.com/YOUR_USERNAME/nhalege-media-ai.git
+git push -u origin main
